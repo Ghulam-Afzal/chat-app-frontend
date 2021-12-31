e04a5c11f88ac2846a693f025fc0d5bb984fb6bb
Ghulam <inferno2960@gmail.com>
2021-12-30 19:56:54 -0500

    edited the signin page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -6,7 +6,6 @@ const signin_url = 'http://127.0.0.1:8080/api/auth/signin'
 const Login = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [errormsg, setErrorMsg] = useState('')
 
 
     const login = async (event) => {
@@ -18,12 +17,9 @@ const Login = () => {
                 password: password
             }
             const user = await axios.post(signin_url, obj)
-            // console.log(user)
-            // console.log(user.data)
             window.localStorage.setItem("loggedinUser", JSON.stringify(user.data))
         } catch (error){
             console.log(error)
-            // setErrorMsg()
         }
         setUsername('')
         setPassword('')
