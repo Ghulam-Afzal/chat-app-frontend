3d27d1057f15cfa964355b660fca9ec87c6df9d4
Ghulam <inferno2960@gmail.com>
2022-01-02 18:05:57 -0500

    added another api endpoint

--- src/service/service.config.js
+++ src/service/service.config.js
@@ -3,6 +3,7 @@ const config = {
     mes_url: 'http://127.0.0.1:8080/api/messages/getMessages',
     g_get_url: 'http://127.0.0.1:8080/api/groups/getGroups',
     g_join_url: "http://127.0.0.1:8080/api/groups/joinGroup",
+    g_leave_url: "http://127.0.0.1:8080/api/groups/leaveGroup",
     g_create_url: "http://127.0.0.1:8080/api/groups/createGroup",
     signup_url: 'http://127.0.0.1:8080/api/auth/signup',
     signin_url: 'http://127.0.0.1:8080/api/auth/signin'
