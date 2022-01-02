097b6d6ed8f865febd35dc2f6361cb83fedb2622
Ghulam <inferno2960@gmail.com>
2022-01-02 18:07:03 -0500

    moved joinGroup and createGroup to user state and added a leaveGroup

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,18 +1,71 @@
 import authService from '../service/auth'
+import groupService from '../service/groups'
 
 
 const userReducer = (state = [], action) => {
     switch(action.type) {
         case "INIT_USER": 
             return action.data
+        case "CREATE_GROUP": 
+            return [...state, action.data]
+        case "JOIN_GROUP":
+            return [...state, action.data]
+        case "LEAVE_GROUP": 
+            return [...state.filter(group => group.groupId !== action.data)]
         default: 
             return state
     }
 }
 
-export const initializeUser = () => {
+
+
+export const createGroup = (owner, groupName, userId) => {
+    const obj = {
+        owner: owner, 
+        groupName: groupName, 
+        userId: userId
+    }
     return async dispatch => {
-        const user = await authService.getUserInfo(5)
+        const newGroup = await groupService.createGroup(obj)
+        dispatch ({
+            type: "CREATE_GROUP", 
+            data: newGroup
+        })
+    }
+}
+
+
+export const joinGroup = (userId, groupId) => {
+    const info = { 
+        userId: userId,
+        id: groupId 
+    }
+    return async dispatch => {
+        const groupJoined = await groupService.joinGroup(info)
+        dispatch({
+            type: "JOIN_GROUP", 
+            data: groupJoined
+        })
+    }
+}
+
+export const leaveGroup = (userId, groupId) => {
+    const info = { 
+        userId: userId,
+        id: groupId
+    }
+    return async dispatch => {
+        const leaveGroup = await groupService.leaveGroup(info)
+        dispatch({
+            type: "LEAVE_GROUP",
+            data: groupId
+        })
+    }
+}
+
+export const initializeUser = (userId) => {
+    return async dispatch => {
+        const user = await authService.getUserInfo(userId)
         dispatch({
             type: "INIT_USER", 
             data: user.groups,
--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -1,47 +1,11 @@
 import groupService from "../service/groups"
 
-
 const groupReducer = (state = [], action) => {
     switch(action.type) {
-        case "JOIN_GROUP":
-            return [...state, action.data]
-        case "CREATE_GROUP":
-            return [...state, action.data]
         case "INIT_GROUPS": 
             return action.data
         default: 
             return state
-    }
-}
-
-
-export const createGroup = (owner, groupName, userId) => {
-    const obj = {
-        owner: owner, 
-        groupName: groupName, 
-        userId: userId
-    }
-    return async dispatch => {
-        const newGroup = await groupService.createGroup(obj)
-        dispatch ({
-            type: "CREATE_GROUP", 
-            data: newGroup
-        })
-    }
-}
-
-
-export const joinGroup = (userId, groupId) => {
-    const info = { 
-        userId: userId,
-        id: groupId 
-    }
-    return async dispatch => {
-        const groupJoined = await groupService.joinGroup(info)
-        dispatch({
-            type: "JOIN_GROUP", 
-            data: groupJoined
-        })
     }
 }
 
