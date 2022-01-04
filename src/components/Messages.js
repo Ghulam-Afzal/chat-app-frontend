085aecc73115482ebbf6518ef7ac89763c253115
Ghulam <inferno2960@gmail.com>
2022-01-03 19:13:50 -0500

    started a message panel

---
+++ src/components/Messages.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react"
+
+
+const Message = ({ messages }) => {
+
+    const messagesRef = useRef(null)
+    const scrollToBottom = () => {
+        messagesRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages])
+
+    if (messages.length === 0) {
+        return (
+            <div>
+                <p>There are no messages in this group</p>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="messages">
+                {messages.map((message) => {
+                    return (
+                        // {message.user.username}
+                    <li key={message.id}> {message.user.username} : {message.message}</li>
+                    )})}
+                    <div ref={messagesRef}/>
+            </div>
+        )
+    }
+}
+
+
+export default Message
