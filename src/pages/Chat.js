b22b6729aa28fff91a95d64f4a3acea471466c77
Ghulam <inferno2960@gmail.com>
2022-01-04 17:36:35 -0500

    added a way to get userid of the signed in user, and for a socket to listen to a socket event

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,7 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from 'react-redux'
 import { initializeUser, createGroup, joinGroup, leaveGroup } from "../reducers/authReducer"
-import { initializeMessages, newMessage } from "../reducers/messageReducer"
+import { initializeMessages, newMessage, socketNewMessage } from "../reducers/messageReducer"
 import { socket } from "../service/socket"
 import "../App.css"
 import Message from "../components/Messages"
@@ -11,34 +11,34 @@ const Chat = () => {
     const userInfo = useSelector(state => state.user)
     const [curGroup, setCurGroup] = useState()
     const [msg, setMessage] = useState("")
-    const messagesRef = useRef(null)
     const dispatch = useDispatch()
     
     let user; 
+    let userId; 
     const data = window.localStorage.getItem("loggedinUser")
     if (data) {
         const loggedInUser = JSON.parse(data)
         user = loggedInUser.username
+        userId = loggedInUser.id
     }
 
+    socket.on("s-message", (message) => {
+        if (message.groupId === curGroup){
+            // dispatch(socketNewMessage(message))
+            console.log(message)
+        }
+    })
+
     useEffect(() => {
-        dispatch(initializeUser(2))
+        dispatch(initializeUser(userId))
     }, [dispatch])
-
-    // const scrollToBottom = () => {
-    //     messagesRef.current?.scrollIntoView({ behavior: "smooth" })
-    // }
-
-    // useEffect(() => {
-    //     scrollToBottom()
-    // }, [messages])
     
     const handleCreate = () => {
         dispatch(createGroup("bob", "usergROUSPDTETST", 2))
     }
 
     const handleJoin = () => {
-        dispatch(joinGroup(2, "7e8e4102-025f-40af-8f94-4615789e2800"))
+        dispatch(joinGroup(3, "5ef7e1f4-a1e5-4600-bc8a-b6f7546660be"))
     }
 
     const handleLeave = () => {
@@ -57,7 +57,7 @@ const Chat = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
         if (msg.length !== 0){
-            dispatch(newMessage(2, msg, 1))
+            dispatch(newMessage(userId, msg, 1))
             setMessage("")
         }
     }
@@ -80,14 +80,6 @@ const Chat = () => {
             <button onClick={handleLeave}>Leave Group</button>
           </div>
           <div className="right">
-              {/* <div className="messages">
-                {messages.map((message) => {
-                        return (
-                            <li key={message.id}> : {message.message}</li>
-                        )
-                    })}
-                    <div ref={messagesRef}/>
-            </div> */}
             <Message messages={messages} />
             <form className="input-from" onClick={handleSubmit}>
                 <input name="msg" type="text" value={msg} onChange={(e) => setMessage(e.target.value)}></input>
