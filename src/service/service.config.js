7761bc273c9c8a4dd3ec22adae06324fd6439edf
Ghulam <inferno2960@gmail.com>
2022-01-03 19:17:17 -0500

    added a new api enpoint

--- src/service/service.config.js
+++ src/service/service.config.js
@@ -1,6 +1,7 @@
 const config = {
     user_url: 'http://127.0.0.1:8080/api/auth/getUser',
     mes_url: 'http://127.0.0.1:8080/api/messages/getMessages',
+    new_mes_url: 'http://127.0.0.1:8080/api/messages/newMessage',
     g_get_url: 'http://127.0.0.1:8080/api/groups/getGroups',
     g_join_url: "http://127.0.0.1:8080/api/groups/joinGroup",
     g_leave_url: "http://127.0.0.1:8080/api/groups/leaveGroup",
