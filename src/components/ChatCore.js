ede9d67b89018377857019337e53794448ecfe75
Ghulam <inferno2960@gmail.com>
2022-01-08 22:20:09 -0500

    renamed original Chat file to ChatCore  and moved it to components folder as it is more of a component now

---
+++ src/components/ChatCore.js
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { initializeUser, createGroup, joinGroup, leaveGroup } from "../reducers/authReducer"
+import { initializeMessages, newMessage, socketNewMessage } from "../reducers/messageReducer"
+import { socket } from "../service/socket"
+import "../App.css"
+import Message from "../components/Messages"
+import GroupPanel from "../components/GroupPanel"
+import JoinGroup from "../components/JoinGroup"
+import LeaveGroup from "../components/LeaveGroup"
+import { IoIosAddCircleOutline } from "react-icons/io";
+import CreateGroupModel from "../components/CreateGroupModel"
+import { initializeGroups } from "../reducers/groupReducer"
+import { handleLoginState } from "../reducers/loginReducer"
+
+
+const Chat = () => {
+    const messages = useSelector(state => state.messages)
+    const userInfo = useSelector(state => state.user)
+    const allGroups = useSelector(state => state.groups)
+    const userLoggedIn = useSelector(state => state.loggedIn)
+    const [curGroup, setCurGroup] = useState()
+    const [msg, setMessage] = useState("")
+    const [show, setShow] = useState(false)
+    const [groupName, setGroupName] = useState('')
+    const dispatch = useDispatch()
+
+    let user; 
+    let userId; 
+    const data = window.localStorage.getItem("loggedinUser")
+    if (data) {
+        const loggedInUser = JSON.parse(data)
+        user = (loggedInUser.username)
+        userId = loggedInUser.id
+    }
+    
+    socket.off('s-message').on("s-message", (message) => {
+        if (message.groupId === curGroup){
+            dispatch(socketNewMessage(message))
+        }
+    })
+
+    useEffect(() => {
+        dispatch(initializeUser(userId))
+        dispatch(initializeGroups())
+    }, [dispatch])
+    
+    const handleCreate = (event) => {
+        event.preventDefault()
+        if (groupName.length > 0){
+            dispatch(createGroup(user, groupName, userId))
+            setGroupName("")
+        }
+    }
+
+    const handleJoin = (groupId) => {
+        dispatch(joinGroup(userId, groupId))
+    }
+
+    const handleLeave = (groupId) => {
+        dispatch(leaveGroup(userId, groupId))
+    }
+
+    const getGroupMessages = (groupId) => {
+        dispatch(initializeMessages(groupId))
+        setCurGroup(groupId)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+            if(curGroup){
+                if (msg.length !== 0){
+                    dispatch(newMessage(userId, msg, curGroup))
+                    setMessage("")
+                }
+        }
+    }
+
+    const handleClose = () => {
+        setShow(false)
+    }
+    const noUserScreen = () => {
+        return (
+            <div>No User Signed In</div>
+        )
+    }
+    
+    const logout = () => {
+        window.localStorage.removeItem("loggedinUser");
+        dispatch(handleLoginState())
+    }
+
+    const chat = () => {
+        return (
+            <div className="contianer">
+                <CreateGroupModel handleClose={handleClose} handleCreate={handleCreate} show={show} groupName={groupName} setGroupName={setGroupName}/>
+                <LeaveGroup groups={userInfo} leaveGroup={handleLeave} />
+                <JoinGroup allGroups={allGroups} userGroups={userInfo} handleJoin={handleJoin}  />
+                <IoIosAddCircleOutline onClick={() => setShow(!show)}/>
+                <GroupPanel username={user} groups={userInfo} handleCreate={handleCreate} handleJoin={handleJoin} handleLeave={handleLeave} getGroupMessages={getGroupMessages}/>
+                <div className="right">
+                  <Message messages={messages} />
+                  <form className="input-from" onClick={handleSubmit}>
+                      <input name="msg" type="text" value={msg} onChange={(e) => setMessage(e.target.value)}></input>
+                      <button type="submit">Send</button>
+                  </form>
+                  <button onClick={logout}>Logout</button>
+              </div>
+            </div>
+          )
+    }
+
+    return (
+        <div>
+            {userLoggedIn === false ? noUserScreen() : chat()}
+        </div>
+    )
+}
+
+export default Chat
