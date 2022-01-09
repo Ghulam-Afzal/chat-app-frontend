ae13b5e4a67a4c0bff4d4a5c9c64c0e85f32e969
Ghulam <inferno2960@gmail.com>
2022-01-09 14:47:24 -0500

    added css to the CreateGroupModel

--- src/components/CreateGroupModel.js
+++ src/components/CreateGroupModel.js
@@ -1,15 +1,18 @@
 import { AiOutlineClose } from "react-icons/ai";
 
 const CreateGroupModel = ({ show, handleClose, groupName, setGroupName, handleCreate}) => {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
+    const showHideClassName = show ?  "modal display-block" : "modal display-none";
 
+    console.log(show)
     return (
         <div className={showHideClassName}>
-            <AiOutlineClose onSubmit={handleClose} />
-            <form onSubmit={handleCreate}> 
-                Group Name: <input value={groupName} onChange={(e) => setGroupName(e.target.value)}/>
-                <button type="submit" >Submit</button>
-            </form>
+            <section  className="modal-main">
+                <AiOutlineClose className="close-btn" onClick={handleClose}/>
+                <form onSubmit={handleCreate}> 
+                    Group Name: <input value={groupName} onChange={(e) => setGroupName(e.target.value)}/>
+                    <button type="submit" >Submit</button>
+                </form>
+            </section>
         </div>
     )
 }
