51d915fcf754fac7b75ff0d592f470a8e5505fb8
Ghulam <inferno2960@gmail.com>
2022-01-09 15:45:53 -0500

    nade the model open only when a show is set to true

--- src/components/LeaveGroup.js
+++ src/components/LeaveGroup.js
@@ -1,13 +1,19 @@
-const LeaveGroup = ({ groups, leaveGroup }) => {
+import { AiOutlineClose } from "react-icons/ai";
+
+const LeaveGroup = ({ show, handleClose, groups, leaveGroup }) => {
+    const showHideClassName = show ?  "modal display-block" : "modal display-none";
     return (
-        <div>
-            {groups.map((group) => {
-                return (
-                    <li key={group.groupId}>
-                        <button onClick={() => leaveGroup(group.groupId)}>{group.name}</button>
-                    </li>
-                )
-            })}
+        <div className={showHideClassName}>
+            <section  className="modal-main">
+            <AiOutlineClose className="close-btn" onClick={handleClose}/>
+                {groups.map((group) => {
+                    return (
+                        <li key={group.groupId}>
+                            <button onClick={() => leaveGroup(group.groupId)}>{group.name}</button>
+                        </li>
+                    )
+                })}
+            </section>
         </div>
     )
 }
