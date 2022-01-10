1d42613ad5a21261cf2dc9929149b472d8cc62c5
Ghulam <inferno2960@gmail.com>
2022-01-10 17:22:20 -0500

    added a feild for auth tokens to be used in the axios request

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 import config from "./service.config";
 
+let token1 = null
+
+export const setToken1 = newToken => {
+    token1 = `bearer ${newToken}`
+}
+
 const getUserInfo = async userId => {
-    const response = await axios.post(config.user_url, {userId: userId})
+    const temp = {
+        headers: { authorization: token1}
+    }
+    const response = await axios.post(config.user_url, {userId: userId}, temp)
     return response.data
 }
 
--- src/service/groups.js
+++ src/service/groups.js
@@ -1,23 +1,38 @@
 import axios from "axios"
 import config from "./service.config"
 
+let token2 = null
+
+export const setToken2 = newToken => {
+    token2 = `bearer ${newToken}`
+}
+
 const getGroups = async () => {
     const response = await axios.post(config.g_get_url)
     return response.data 
 }
 
 const createGroup = async info => {
-    const response = await axios.post(config.g_create_url, info)
+    const temp = {
+        headers: { authorization: token2}
+    }
+    const response = await axios.post(config.g_create_url, info, temp)
     return response.data
 }
 
 const joinGroup = async info => {
-    const response =  await axios.put(config.g_join_url, info)
+    const temp = {
+        headers: { authorization: token2}
+    }
+    const response =  await axios.put(config.g_join_url, info, temp)
     return response.data
 }
 
 const leaveGroup = async info => {
-    const response =  await axios.put(config.g_leave_url, info)
+    const temp = {
+        headers: { authorization: token2}
+    }
+    const response =  await axios.put(config.g_leave_url, info, temp)
     return response.data
 }
 
