68dcd94e8300ae299dbabfad06a5183e61c05c2b
Ghulam <inferno2960@gmail.com>
2022-01-09 20:28:09 -0500

    added a check to see if there are groups that can be joined to left for the user

--- src/components/JoinGroup.js
+++ src/components/JoinGroup.js
@@ -4,22 +4,42 @@ const JoinGroup = ({ show, handleClose, allGroups, userGroups, handleJoin }) => 
     const showHideClassName = show ?  "modal display-block" : "modal display-none";
 
     const temp = allGroups.filter((group) => !userGroups.find(({ groupId }) => group.groupId === groupId))
+
+    const hasRoomsJoinable = () => {
+        return (
+            <div className={showHideClassName}>
+                <section  className="list-model">
+                <AiOutlineClose className="close-btn" onClick={handleClose}/>
+                    <h3>Groups Joinable</h3>
+                    <ul>
+                        {temp.map((group) => {
+                            return (
+                            <li className="group-item" key={group.id}>
+                                <p>{group.name}</p>
+                                <button onClick={() => handleJoin(group.groupId)}>Join</button>
+                            </li>
+                            )
+                        })}
+                    </ul>
+                </section>
+            </div>
+        )
+    }
+
+    const noRooms = () => {
+        return (
+            <div className={showHideClassName}>
+                <section  className="list-model">
+                    <AiOutlineClose className="close-btn" onClick={handleClose}/>
+                    <p>There are no rooms that you can join.</p>
+                </section>
+            </div>
+        )
+    }
+
     return (
-        <div className={showHideClassName}>
-            <section  className="list-model">
-            <AiOutlineClose className="close-btn" onClick={handleClose}/>
-                <h3>Groups Joinable</h3>
-                <ul>
-                    {temp.map((group) => {
-                        return (
-                        <li className="group-item" key={group.id}>
-                            <p>{group.name}</p>
-                            <button onClick={() => handleJoin(group.groupId)}>Join</button>
-                        </li>
-                        )
-                    })}
-                </ul>
-            </section>
+        <div>
+            {temp === null ? noRooms() : hasRoomsJoinable()}
         </div>
     )
 }
--- src/components/LeaveGroup.js
+++ src/components/LeaveGroup.js
@@ -2,22 +2,42 @@ import { AiOutlineClose } from "react-icons/ai";
 
 const LeaveGroup = ({ show, handleClose, groups, leaveGroup }) => {
     const showHideClassName = show ?  "modal display-block" : "modal display-none";
+    
+    const hasRoomsLeavable = () => {
+        return (
+            <div className={showHideClassName}>
+                <section  className="list-model">
+                <AiOutlineClose className="close-btn" onClick={handleClose}/>
+                <h3>Groups Leavable</h3>
+                    <ul>
+                        {groups.map((group) => {
+                            return (
+                                <li className="group-item" key={group.groupId}>
+                                    <p>{group.name}</p>
+                                    <button onClick={() => leaveGroup(group.groupId)}>Leave</button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </section>
+            </div>
+        )
+    }
+
+    const noRooms = () => {
+        return (
+            <div className={showHideClassName}>
+                <section  className="list-model">
+                    <AiOutlineClose className="close-btn" onClick={handleClose}/>
+                    <p>There are no rooms that you can leave.</p>
+                </section>
+            </div>
+        )
+    }
+
     return (
-        <div className={showHideClassName}>
-            <section  className="list-model">
-            <AiOutlineClose className="close-btn" onClick={handleClose}/>
-            <h3>Groups Leavable</h3>
-                <ul>
-                    {groups.map((group) => {
-                        return (
-                            <li className="group-item" key={group.groupId}>
-                                <p>{group.name}</p>
-                                <button onClick={() => leaveGroup(group.groupId)}>Leave</button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </section>
+        <div>
+            {groups === null ? noRooms() : hasRoomsLeavable()}
         </div>
     )
 }
