a52cc5c8e4aa08e2a6356d1e949d2297edbb868a
Ghulam <inferno2960@gmail.com>
2022-01-16 19:52:07 -0500

    added config for build

--- src/service/service.config.js
+++ src/service/service.config.js
@@ -10,4 +10,18 @@ const config = {
     signin_url: 'http://127.0.0.1:8080/api/auth/signin'
 }
 
+
+// meant for when the react front end is buit and used in the backend asn on app
+// const config = {
+//     user_url: '/api/auth/getUser',
+//     mes_url: '/api/messages/getMessages',
+//     new_mes_url: '/api/messages/newMessage',
+//     g_get_url: '/api/groups/getGroups',
+//     g_join_url: "/api/groups/joinGroup",
+//     g_leave_url: "/api/groups/leaveGroup",
+//     g_create_url: "/api/groups/createGroup",
+//     signup_url: '/api/auth/signup',
+//     signin_url: '/api/auth/signin'
+// }
+
 export default config
